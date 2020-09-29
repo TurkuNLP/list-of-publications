f5c5ea3ee39bff297dd2ff79124bcd942b024dc6
Filip Ginter <ginter@cs.utu.fi>
2020-09-29 15:25:15 +0300

    support for related work

--- make_lops.sh
+++ make_lops.sh
@@ -23,7 +23,8 @@ function make_lop {
 }
 
 #       directory  latexname   search
-make_lop ginter_lop "Filip Ginter" Ginter
+FILIP_AKA_2020="kanerva2020lemmatizer,mehryary2020pairs,ronnqvist2019bert,kanerva2019newsgen,udst:turkunlp,kyrolainen2017autoencoder,luotolahti-kanerva-ginter:2017:DiscoMT,moen2017pain,conll2017kanerva,luotolahti2015parsebanks"
+make_lop ginter_lop "Filip Ginter" Ginter --rel $FILIP_AKA_2020
 make_lop kanerva_lop "Jenna Kanerva (Nyblom)" Kanerva Nyblom
 make_lop mehryary_lop "Farrokh Mehryary" Mehryary
 make_lop luotolahti_lop "Juhani Luotolahti" Luotolahti
